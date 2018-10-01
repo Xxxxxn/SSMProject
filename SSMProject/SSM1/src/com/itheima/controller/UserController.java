b13f2c88cbb2271a4f02fec08db33b9c1d5c4377
Xn <chengen1228@163.com>
2018-10-01 13:27:26 +0800

    添加 SSM1项目

---
+++ SSMProject/SSM1/src/com/itheima/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.itheima.controller;
+
+import com.itheima.pojo.User;
+import com.itheima.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+@ResponseBody
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @RequestMapping("findAll")
+    public List<User> findAll(){
+        List<User> userList = userService.findAll();
+        return userList;
+    }
+}
---
+++ SSMProject/SSM1/src/com/itheima/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.itheima.mapper;
+
+import com.itheima.pojo.User;
+
+import java.util.List;
+public interface UserMapper {
+    public List<User> findAll();
+}
